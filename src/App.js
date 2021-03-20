bffc85cdb3ed0990f1524d2f7809f26c970f1f87
asaniraju <asani1813@gmail.com>
2021-03-20 18:22:49 +0530

    added sidebar menu changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
 import './App.css';
+import NavBar from './components/Navbar/Navbar'
+import Home from './pages/Home';
+import Messages from './pages/Messages';
+import Products from './pages/Products';
+import Reports from './pages/Reports';
+import Support from './pages/Support';
+import Teams from './pages/Teams';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <NavBar/>
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/reports" component={Reports}/>
+        <Route path="/products" component={Products}/>
+        <Route path="/team" component={Teams}/>
+        <Route path="/messages" component={Messages}/>
+        <Route path="/support" component={Support}/>
+      </Switch>
+    </Router>
+    </>
   );
 }
 
 export default App;
+ 
---
+++ src/pages/Teams.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Teams() {
+    return (
+        <div className="teams">
+            Teams
+        </div>
+    )
+}
+
+export default Teams
